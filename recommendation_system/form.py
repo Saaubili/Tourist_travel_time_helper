3268b9ecb9d850a42f57e0b9448d6428f5fefe5a
Saaubili <sarikaubili@gmail.com>
2026-01-20 11:17:11 +0700

    deleted useless js and form files

--- recommendation_system/form.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from data_receiver.models import City
-
-
-class CitySelectForm(forms.Form):
-    city = forms.ModelChoiceField(
-        queryset=City.objects.all(),
-        widget=forms.HiddenInput()
-    )
